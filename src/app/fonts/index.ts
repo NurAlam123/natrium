cc1aceb951f6fc30f0267ca6aaf333d269616983
Nur <nuralam.rsc@gmail.com>
2024-11-03 23:40:42 +0600

    fix: add subsets to font

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -2,10 +2,12 @@ import { Poppins, Bebas_Neue } from "next/font/google";
 
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
   variable: "--font-poppins",
 });
 
 export const bebasNeue = Bebas_Neue({
   weight: ["400"],
+  subsets: ["latin"],
   variable: "--font-bebas-neue",
 });
