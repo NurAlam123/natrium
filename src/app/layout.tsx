592371dd6d6004a25fa7948c870eaaee86f95744
Nur <nuralam.rsc@gmail.com>
2024-11-17 23:23:18 +0600

    fix: fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,54 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { poppins } from "./fonts";
 import { Toaster } from "react-hot-toast";
+import { meta } from "./config";
 
 export const metadata: Metadata = {
-  title: "Nur Alam",
-  description: "Hi! I am Nur Alam. I am a Front-End Developer.",
+  metadataBase: new URL(meta.site.url),
+  title: {default: meta.site.title, template: `%s | ${meta.site.title}`},
+  description: meta.site.description,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1
+    }
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: meta.author.name,
+    description: meta.site.description,
+    images: [
+      {
+        url: meta.site.bannerImage,
+        width: 1280,
+        height: 720,
+        alt: meta.site.description
+      }
+    ]
+  },
+  keywords: meta.site.keywords,
+  creator: meta.author.username,
+  openGraph: {
+    type: "website",
+    url: meta.site.url,
+    title: meta.site.title,
+    description: meta.site.description,
+    locale: "en-US",
+    images: [
+      {
+        url: meta.site.bannerImage,
+        width: 1280,
+        height: 720,
+        alt: meta.site.description
+      }
+    ]
+  }
+
 };
 
 export default function RootLayout({
