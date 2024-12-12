eba8a5ab703d658c26cf07fba19ebc393b8ed6a9
Nur <nuralam.rsc@gmail.com>
2024-12-12 19:29:47 +0600

    fix(navbar): fix navbar style and animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,42 +2,57 @@
 
 import Link from "next/link";
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Navbar = () => {
-  const [isHidden, setIsHidden] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const navRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const { scrollY } = useScroll();
 
   useMotionValueEvent(scrollY, "change", (y) => {
-    if (y > 100) {
-      setIsHidden(true);
-    } else {
-      setIsHidden(false);
+    const containerPos = containerRef.current?.getBoundingClientRect();
+    const top = containerPos?.top;
+
+    if (!top) return;
+    const bottom = document.documentElement.scrollHeight - top - 240;
+
+    if (y > top && !show && y < bottom) {
+      setShow(true);
+      return;
+    }
+
+    if ((y <= top || y > bottom) && show) {
+      setShow(false);
+      return;
     }
   });
 
   return (
-    <motion.div
-      className="hidden md:flex justify-center items-center fixed top-full left-0 z-10 ms-5 pb-4 shadow-sm"
+    <motion.section
+      ref={containerRef}
+      className="hidden md:flex justify-center items-center fixed top-full z-10 ms-5 pb-4 shadow-sm"
       variants={{
-        hidden: {
+        visible: {
           y: "-100%",
         },
-        visible: {
+        hidden: {
           y: "0%",
         },
       }}
-      animate={isHidden ? "hidden" : "visible"}
+      animate={show ? "visible" : "hidden"}
       transition={{
-        duration: 0.2,
+        duration: 0.5,
+        ease: "easeInOut",
       }}
     >
-      <motion.nav className="bg-dark border-dark-2 border p-4 text-sm z-10 *:text-primary *:border *:border-dark-1 *:bg-dark-1 *:px-4 *:py-2 *:rounded-2xl rounded-xl flex flex-col gap-2">
-        <Link
-          href="/"
-          className="hover:bg-dark/20 hover:border hover:border-dark-2"
-        >
+      <motion.nav
+        ref={navRef}
+        className="bg-dark border-dark-2 border p-4 text-sm z-10 *:text-secondary *:font-medium *:bg-dark-1 *:px-4 *:py-2 *:rounded-2xl rounded-xl flex flex-col gap-2 *:border *:border-dark-2 *:transition *:text-center"
+      >
+        <Link href="/" className="hover:bg-dark/20">
           Home
         </Link>
         <Link href="#about" className="hover:bg-dark/20">
@@ -50,7 +65,7 @@ const Navbar = () => {
           Contact
         </Link>
       </motion.nav>
-    </motion.div>
+    </motion.section>
   );
 };
 
