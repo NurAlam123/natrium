a611f8814563faa62ce28ef724ee0b32e7f534bc
Nur <nuralam.rsc@gmail.com>
2025-01-12 21:14:38 +0600

    fix: fix footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="py-16 flex justify-center items-center text-gray/70 border-t border-gray/50">
+    <footer className="py-8 md:py-16 flex justify-center items-center text-daisy/70 border-t border-daisy/50">
       <div className="space-y-4">
-        <div className="flex items-center justify-between gap-2">
-          <p>&copy; 2024 - 2025. Nur Alam.</p>
+        <div className="flex flex-col items-center justify-between gap-2">
+          <p>Nur Alam &copy; 2024 - 2025.</p>
           <p>All rights reserved.</p>
         </div>
         <div className="flex gap-2 items-center justify-center text-primary font-medium">
@@ -16,7 +16,7 @@ const Footer = () => {
               href={navLink.href}
               key={navLink.title}
             >
-              <span className="hover:text-gray">{navLink.title}</span>
+              <span className="hover:text-primary/80">{navLink.title}</span>
               <span className="group-last:hidden">/</span>
             </Link>
           ))}
