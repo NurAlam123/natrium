6a209d581bd116647ec422e84cdae4adf7c38b1b
Nur <nuralam.rsc@gmail.com>
2025-01-16 04:10:55 +0600

    feat: add modal popup animation

--- src/components/Hero/NameAndAddress.tsx
+++ src/components/Hero/NameAndAddress.tsx
@@ -12,7 +12,7 @@ const NameAndAddress = () => {
           <TextFlip>Dhaka, Bangladesh</TextFlip>
         </address>
       </div>
-      <h1 className="font-bebas-neue font-semibold text-center not-italic sm:text-[64px] text-[81px] md:text-[258px] tracking-wider text-secondary !leading-tight">
+      <h1 className="font-bebas-neue font-semibold text-center not-italic sm:text-[64px] text-[81px] md:text-[258px] tracking-wider text-secondary !leading-tight [font-display:swap]">
         <HeroTextFancyAnimation>Nur Alam</HeroTextFancyAnimation>
       </h1>
     </div>
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,25 +1,77 @@
+"use client";
+
 import { IoClose } from "react-icons/io5";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import { cn } from "@/lib/utils";
 import useModalStore from "@/store/modal-store";
 import useModalEsc from "@/hooks/useModalEsc";
 
-const Modal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const modalVariant: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: { opacity: 0, transition: { delay: 0.3, duration: 0.8 } },
+};
+
+const contentVariant: Variants = {
+  initial: {
+    scale: 0.4,
+    opacity: 0,
+  },
+  animate: {
+    scale: 1,
+    opacity: 1,
+  },
+  exit: {
+    scale: 0,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Modal: React.FC<Props> = ({ children }) => {
   const showModal = useModalStore((state) => state.showModal);
   const closeModal = useModalStore((state) => state.closeModal);
   const toggleModal = useModalStore((state) => state.toggleModal);
   useModalEsc(closeModal, showModal);
 
   return (
-    <>
+    <AnimatePresence mode="wait">
       {showModal && (
         <motion.div
+          key="modal"
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={modalVariant}
+          transition={{
+            duration: 0.4,
+          }}
           className={cn(
             "w-full h-full top-0 left-0 bg-dark/90 backdrop-blur fixed justify-center items-center z-[20] flex",
           )}
         >
           <div className="absolute inset-0" onClick={closeModal} />
-          <div className="relative max-w-screen-md rounded-xl border border-dark-3 bg-dark h-[90%] mx-2 p-6 z-[22]">
+          <motion.div
+            key="modal-content"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={contentVariant}
+            transition={{
+              duration: 0.6,
+            }}
+            className="relative max-w-screen-md rounded-xl border border-dark-3 bg-dark h-[90%] mx-2 p-6 z-[22]"
+          >
             <button
               onClick={toggleModal}
               className="absolute flex justify-center items-center top-0 right-0 w-6 h-6 bg-daisy text-dark mx-1 my-1 rounded-full"
@@ -27,10 +79,10 @@ const Modal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
               <IoClose />
             </button>
             <div className="max-w-full h-full py-4">{children}</div>
-          </div>
+          </motion.div>
         </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 };
 
